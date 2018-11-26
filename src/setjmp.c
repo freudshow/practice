9c69463d1d17a2e1eae46ce87af344a275c9c171
s_baoshan <s_baoshan@hotmail.com>
2018-11-26 14:42:43 +0800

    setjmp

---
+++ src/setjmp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <setjmp.h>
+jmp_buf buf;
+
+int banana()
+{
+	printf("in banana() \n");
+	longjmp(buf, 1);
+
+	printf("you'll never see this,because i longjmp'd");
+
+}
+
+int main()
+{
+	if (setjmp(buf))
+		printf("back in main\n");
+	else {
+		printf("first time through\n");
+		banana();
+	}
+
+}
